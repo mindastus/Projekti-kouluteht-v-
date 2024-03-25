28bf59259b0c1d91d1f3fcaf28fbff3712908805
Minttu <141176978+mindastus@users.noreply.github.com>
2024-03-25 13:09:09 +0200

    Add files via upload

---
+++ demo8/kirjautuminen.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut() {
+if (localStorage.getItem("kirjautunut") === 'kylla') {
+    document.getElementById('tervetulo_teksti').textContent += localStorage.getItem("nimi");
+    document.getElementById("kirjautumis_lomake").style.display = "none";
+    }
+}
+
+function kirjaudu(){
+    localStorage.setItem("nimi", document.getElementById("nimi").value)
+    localStorage.setItem("kirjautunut","kylla")
+}
